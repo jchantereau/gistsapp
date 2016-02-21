3fd6c6926c9e01a0fe0e83021d5d711efde74b57
Julien Chantereau <jchantereau@gmail.com>
2016-02-21 13:14:56 +0100

    models and routes setup

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,14 +1,51 @@
 class GistsController < ApplicationController
 
+  before_action :find_gist, only: [:show, :edit, :update, :destroy]
+
   def index
     @gists = Gist.all
   end
 
   def show
+  end
+
+  def new
+    @gist = Gist.new
+  end
+
+  def create
+    @gist = Gist.new(gist_params)
+    if @gist.save
+      redirect_to gist_path(@gist)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @gist.update(gist_params)
+    if @gist.save
+      redirect_to gist_path(@gist)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gist.destroy
+  end
+
+  private
+
+  def find_gist
     @gist = Gist.find(params[:id])
   end
 
-  def
-
+  def gist_params
+    params.require(:gist).permit(:name, :description, :content)
+  end
 
 end
